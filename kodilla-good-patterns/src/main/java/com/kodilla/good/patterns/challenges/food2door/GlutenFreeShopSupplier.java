ddb64d21eb556a896f25061e4c8314d85a8eb7cf
Barhwald <bylinabass@gmail.com>
2023-04-29 13:11:30 +0200

    29.04 13.4 Food2Door

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/GlutenFreeShopSupplier.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/GlutenFreeShopSupplier.java
@@ -3,6 +3,7 @@ package com.kodilla.good.patterns.challenges.food2door;
 public class GlutenFreeShopSupplier implements FoodSupplier {
 
     private final String name = "Gluten Free Shop";
+
     @Override
     public void process(Product product) {
         System.out.printf("""
