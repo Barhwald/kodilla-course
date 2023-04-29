13cd6064f4c04619151559fa8741c3516f5c5962
Barhwald <bylinabass@gmail.com>
2023-04-29 13:07:03 +0200

    29.04 13.2 OrderService classes refactored

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/ExtraFoodShopSupplier.java
@@ -0,0 +1,10 @@
+package com.kodilla.good.patterns.challenges.food2door;
+
+public class ExtraFoodShop implements FoodSupplier {
+
+    private final String name = "Extra Food Shop";
+    @Override
+    public void process() {
+        System.out.println();
+    }
+}
