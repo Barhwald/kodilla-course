33556ad47ff51a6798b629195fc4ca0522e2c63a
Barhwald <bylinabass@gmail.com>
2023-05-14 15:12:27 +0200

    14.05 15.6 Fibonacci recurrent

--- kodilla-spring/src/main/java/com/kodilla/spring/KodillaSpringApplication.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/KodillaSpringApplication.java
@@ -8,13 +8,20 @@ public class KodillaSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaSpringApplication.class, args);
-        System.out.println(fibonacci(4));
-        System.out.println(fibonacci(5));
-        System.out.println(fibonacci(6));
-        System.out.println(fibonacci(7));
-        System.out.println(fibonacci(8));
-        System.out.println(fibonacci(9));
-        System.out.println(fibonacci(10));
+//        System.out.println(fibonacci(4));
+//        System.out.println(fibonacci(5));
+//        System.out.println(fibonacci(6));
+//        System.out.println(fibonacci(7));
+//        System.out.println(fibonacci(8));
+//        System.out.println(fibonacci(9));
+//        System.out.println(fibonacci(10));
+        System.out.println(recurrentFibonacci(1));
+        System.out.println(recurrentFibonacci(2));
+        System.out.println(recurrentFibonacci(3));
+        System.out.println(recurrentFibonacci(4));
+        System.out.println(recurrentFibonacci(5));
+
+
     }
 
 
@@ -41,6 +48,10 @@ public class KodillaSpringApplication {
 
     }
 
-
-
+    public static int recurrentFibonacci(int n) {
+        if (n < 3) {
+            return 1;
+        }
+        return recurrentFibonacci(n-2) + recurrentFibonacci(n-1);
+    }
 }
