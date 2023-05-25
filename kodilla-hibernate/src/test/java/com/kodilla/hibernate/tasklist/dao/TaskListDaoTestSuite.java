05c11f24af1a8991408dd0fe91116076c5f6628f
Barhwald <bylinabass@gmail.com>
2023-05-25 20:24:48 +0200

    25.05 17.2 Tasklist entity creation

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    void testFindByListName() {
+        //given
+        TaskList taskList = new TaskList("In progress", "Tasks not finished yet");
+
+        //when
+        taskListDao.save(taskList);
+
+        //then
+        String name = taskList.getListName();
+        Optional<TaskList> optionalTaskList = taskListDao.findByListName(name);
+        assertTrue(optionalTaskList.isPresent());
+
+        //cleanUp
+        taskListDao.delete(taskList);
+
+    }
+
+}
