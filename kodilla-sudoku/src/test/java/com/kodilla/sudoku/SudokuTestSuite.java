831d1cf39dc05498ec5f3bfb0615cfa87861aa94
Barhwald <bylinabass@gmail.com>
2023-05-26 21:27:16 +0200

    26.05 16.8 Tests for Sudoku improved

--- kodilla-sudoku/src/test/java/com/kodilla/sudoku/SudokuTestSuite.java
+++ kodilla-sudoku/src/test/java/com/kodilla/sudoku/SudokuTestSuite.java
@@ -3,6 +3,8 @@ package com.kodilla.sudoku;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class SudokuTestSuite {
 
     private final SudokuGame sudokuGame = new SudokuGame();
@@ -56,6 +58,55 @@ public class SudokuTestSuite {
             {9, 0, 0, 0, 8, 6, 7, 0, 0}
     };
 
+    private final int[][] solvedEasyBoard = {
+            {4, 2, 7, 8, 6, 5, 3, 9, 1},
+            {9, 1, 5, 2, 4, 3, 7, 6, 8},
+            {6, 8, 3, 7, 9, 1, 4, 2, 5},
+            {5, 9, 8, 4, 3, 7, 2, 1, 6},
+            {7, 6, 4, 9, 1, 2, 8, 5, 3},
+            {1, 3, 2, 5, 8, 6, 9, 4, 7},
+            {8, 7, 1, 6, 2, 9, 5, 3, 4},
+            {3, 4, 9, 1, 5, 8, 6, 7, 2},
+            {2, 5, 6, 3, 7, 4, 1, 8, 9}
+    };
+
+    private final int[][] solvedMediumBoard = {
+            {9, 3, 1, 4, 2, 7, 5, 6, 8},
+            {6, 7, 8, 9, 1, 5, 3, 4, 2},
+            {2, 4, 5, 6, 8, 3, 1, 9, 7},
+            {4, 9, 7, 1, 3, 2, 6, 8, 5},
+            {5, 8, 3, 7, 6, 4, 2, 1, 9},
+            {1, 2, 6, 5, 9, 8, 7, 3, 4},
+            {7, 6, 2, 3, 4, 9, 8, 5, 1},
+            {3, 5, 4, 8, 7, 1, 9, 2, 6},
+            {8, 1, 9, 2, 5, 6, 4, 7, 3}
+    };
+
+    private final int[][] solvedExpertBoard = {
+            {5, 6, 8, 2, 4, 7, 9, 3, 1},
+            {3, 4, 2, 1, 9, 5, 6, 7, 8},
+            {1, 9, 7, 8, 6, 3, 2, 4, 5},
+            {6, 8, 5, 3, 1, 2, 4, 9, 7},
+            {7, 3, 4, 9, 5, 8, 1, 2, 6},
+            {2, 1, 9, 6, 7, 4, 5, 8, 3},
+            {9, 2, 6, 7, 8, 1, 3, 5, 4},
+            {8, 5, 1, 4, 3, 9, 7, 6, 2},
+            {4, 7, 3, 5, 2, 6, 8, 1, 9}
+    };
+
+    private final int[][] solvedEvilBoard = {
+            {4, 7, 9, 2, 6, 5, 3, 1, 8},
+            {5, 3, 1, 9, 7, 8, 4, 6, 2},
+            {8, 6, 2, 1, 3, 4, 5, 7, 9},
+            {1, 8, 7, 6, 5, 9, 2, 4, 3},
+            {3, 9, 4, 8, 1, 2, 6, 5, 7},
+            {2, 5, 6, 7, 4, 3, 9, 8, 1},
+            {7, 2, 5, 4, 9, 1, 8, 3, 6},
+            {6, 4, 8, 3, 2, 7, 1, 9, 5},
+            {9, 1, 3, 5, 8, 6, 7, 2, 4}
+    };
+
+
     private void setGameBoard(int[][] board) {
         for (int i = 0; i < SudokuGame.SIZE; i++) {
             for (int j = 0; j < SudokuGame.SIZE; j++) {
@@ -66,57 +117,67 @@ public class SudokuTestSuite {
         }
     }
 
+    private boolean areBoardsEqual(int[][] board, int[][] solvedBoard) {
+        for (int i = 0; i < SudokuGame.SIZE; i++) {
+            for (int j = 0; j < SudokuGame.SIZE; j++) {
+                if (board[i][j] != solvedBoard[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     @Test
-    void solveEasyGrid() {
+    void solveEasyBoard() {
         //given
         setGameBoard(easyBoard);
-        dataPrinter.printBoard(sudokuGame.getBoard());
+        sudokuSolver.solveBoard(sudokuGame.getBoard());
 
         //when
-        boolean finished = sudokuSolver.solveBoard(sudokuGame.getBoard());
+        boolean result = areBoardsEqual(sudokuGame.getBoard(), solvedEasyBoard);
 
         //then
-        Assertions.assertTrue(finished);
+        Assertions.assertEquals(true, result);
     }
 
     @Test
-    void solveMediumGrid() {
+    void solveMediumBoard() {
         //given
         setGameBoard(mediumBoard);
-        dataPrinter.printBoard(sudokuGame.getBoard());
+        sudokuSolver.solveBoard(sudokuGame.getBoard());
 
         //when
-        boolean finished = sudokuSolver.solveBoard(sudokuGame.getBoard());
+        boolean result = areBoardsEqual(sudokuGame.getBoard(), solvedMediumBoard);
 
         //then
-        Assertions.assertTrue(finished);
-
+        Assertions.assertEquals(true, result);
     }
 
     @Test
-    void solveExpertGrid() {
+    void solveExpertBoard() {
         //given
         setGameBoard(expertBoard);
-        dataPrinter.printBoard(sudokuGame.getBoard());
+        sudokuSolver.solveBoard(sudokuGame.getBoard());
 
         //when
-        boolean finished = sudokuSolver.solveBoard(sudokuGame.getBoard());
+        boolean result = areBoardsEqual(sudokuGame.getBoard(), solvedExpertBoard);
 
         //then
-        Assertions.assertTrue(finished);
+        Assertions.assertEquals(true, result);
     }
 
     @Test
-    void solveEvilGrid() {
+    void solveEvilBoard() {
         //given
         setGameBoard(evilBoard);
-        dataPrinter.printBoard(sudokuGame.getBoard());
+        sudokuSolver.solveBoard(sudokuGame.getBoard());
 
         //when
-        boolean finished = sudokuSolver.solveBoard(sudokuGame.getBoard());
+        boolean result = areBoardsEqual(sudokuGame.getBoard(), solvedEvilBoard);
 
         //then
-        Assertions.assertTrue(finished);
+        Assertions.assertEquals(true, result);
     }
 
 }
